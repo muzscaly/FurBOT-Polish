39faaec122299d35d46f3bbd62850480c9128940
Muzscaly <davivad055@gmail.com>
2020-07-01 22:43:13 +0200

    Update misc.py

--- tg_bot/modules/misc.py
+++ tg_bot/modules/misc.py
@@ -41,7 +41,8 @@ QUOTE_STRINGS = (
     "Płytki podłogowe\n~ Pastel",
     "Chuj z butami, uwaga Olive będzie skakać!\n~ Cytrynek",
     "Nie zesraj sie\n~ Stripe",
-    "Reee znów się zjebałem z łóżka psia kurwa zajebana mać ~ Pusz"
+    "Reee znów się zjebałem z łóżka psia kurwa zajebana mać ~ Pusz",
+    "Jeżeli się mówi \"smacznego\" jak sie zaczyna jeść, to kiedy sie znaczyna pić to mówi sie \"pijnego\"?\n~Olive"
 )
 
 
