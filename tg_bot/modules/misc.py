a512581b8dd35a4598347dcbc08a892c97364f4b
Muzscaly <davivad055@gmail.com>
2020-07-01 20:22:20 +0200

    Update misc.py

--- tg_bot/modules/misc.py
+++ tg_bot/modules/misc.py
@@ -38,7 +38,7 @@ QUOTE_STRINGS = (
     "Epic\n~ Fluffy Incluatio",
     "Mursuit is just a fursuit with (reverse) gloryhole feature\n~ Davis",
     "Jeśli wszystko inne zawiedzie, rzucaj w to Futrzakami puki nie przestanie\n~Lexis UwU",
-    "Płytki podłogowe\n~ Pastel",
+    "Płytki podłogowe\n~ Pastel"
 
 )
 
