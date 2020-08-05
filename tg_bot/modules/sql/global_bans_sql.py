01b79d9d8cd50a763193be6806e3e0c5f9242c5a
Muzscaly <davivad055@gmail.com>
2020-08-05 22:45:50 +0200

    Update global_bans_sql.py

--- tg_bot/modules/sql/global_bans_sql.py
+++ tg_bot/modules/sql/global_bans_sql.py
@@ -56,7 +56,7 @@ def gban_user(user_id, name, reason=None):
             user.name = name
             user.reason = reason
 
-        SESSION.merge(user)
+        SESSION.add(user)
         SESSION.commit()
         __load_gbanned_userid_list()
 
